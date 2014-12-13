a7a1aa6d0c566a817c121aa3e56bd38197f265e4
xcjs <zlowery@xcjs.com>
2014-12-13 18:47:50 -0500

    Added API scaffolding for users and rooms.

---
+++ api/controllers/RoomController.js
@@ -0,0 +1,11 @@
+/**
+ * RoomController
+ *
+ * @description :: Server-side logic for managing Rooms
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
