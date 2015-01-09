f16900e5a52396fa31251844a478b723dde5e2c4
xcjs <zlowery@xcjs.com>
2015-01-08 19:52:47 -0500

    Made corrections for better provisioning automation.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,9 +1,9 @@
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install git nodejs nodejs-legacy npm
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get upgrade -y
+sudo apt-get install -y git nodejs nodejs-legacy npm
 sudo npm install -g sails
 sudo npm install -g grunt-cli
 sudo npm install -g bower
 git clone https://github.com/xcjs/courierchat.git
 cd courierchat/www/
 npm install
-bower install
+bower install --config.interactive=false
