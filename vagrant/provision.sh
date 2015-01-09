327e1e0b250edb9145c5cb7ca8ad9aa229fc7f8f
xcjs <zlowery@xcjs.com>
2015-01-08 19:17:18 -0500

    Corrected the Vagrantbox and attempted an initial provision script for CourierChat.

---
+++ vagrant/provision.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install git nodejs nodejs-legacy npm
+sudo npm install -g sails
+sudo npm install -g grunt-cli
+sudo npm install -g bower
+git clone https://github.com/xcjs/courierchat.git
+cd courierchat/www/
+npm install
+bower install
