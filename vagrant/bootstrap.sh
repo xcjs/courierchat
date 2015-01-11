c2bd708de78d7261c6c3e4d7a11d892be9858037
xcjs <zlowery@xcjs.com>
2015-01-10 19:24:34 -0500

    Split provision script tasks between the root and vagrant user.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,9 +1,25 @@
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get upgrade -y
-sudo apt-get install -y git nodejs nodejs-legacy npm
-sudo npm install -g sails
-sudo npm install -g grunt-cli
-sudo npm install -g bower
-git clone https://github.com/xcjs/courierchat.git
-cd courierchat/www/
-npm install
-bower install --config.interactive=false
+#!/bin/bash
+
+case $(id -u) in
+    0) 
+     	echo "Running root user provisioning..."
+     	apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+		apt-get install -y git nodejs nodejs-legacy npm
+		npm install -g sails
+		npm install -g grunt-cli
+		npm install -g bower
+		
+     	# Execute this script as the vagrant user once root provisioning is
+     	# completed.
+     	sudo -u vagrant -i $0 
+ 	;;
+    *) 
+     	echo "Running vagrant user provisioning..."
+     	git clone https://github.com/xcjs/courierchat.git
+		cd courierchat/www/
+		npm install
+		bower install --config.interactive=false
+ 	;;
+esac
+
+
