ecf08d1c87b969b2104d73aac6dea74e1b244740
xcjs <zlowery@xcjs.com>
2015-03-06 21:28:11 -0500

    Compromised solution between cool animation effects and a touch-friendly interface for the navigation links.

--- www/assets/js/courierchat/directives/cssTouchAssistant.js
+++ www/assets/js/courierchat/directives/cssTouchAssistant.js
@@ -1,9 +1,16 @@
 angular.module('cssTouchAssistant', [])
-.directive('endTouchFocus', [function() {
+.directive('endTouchFocus', ['$window', function($window) {
 	var endTouch = function(scope, e, attrs) {
-		e.on({'touchend': function() {
-			this.blur();
-		}});
+		e.on('click', function() {
+			var touchedElement = angular.element(this);
+			var parent = touchedElement.parent();
+			var timeoutCount = 3000;
+
+			touchedElement.css("animation-play-state", "paused");
+			touchedElement.children().css("animation-play-state", "paused");
+			touchedElement[0].blur();
+			$window.setTimeout(function() { touchedElement.css("animation-play-state", "running"); touchedElement.children().css("animation-play-state", "running"); }, timeoutCount);
+		});
 	};
 
 	return {
