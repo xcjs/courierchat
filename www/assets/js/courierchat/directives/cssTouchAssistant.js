a03de4ceb87b328c7a335d33eb12297325ebde93
xcjs <zlowery@xcjs.com>
2015-03-06 21:58:42 -0500

    Changed click event to be touchend only.

--- www/assets/js/courierchat/directives/cssTouchAssistant.js
+++ www/assets/js/courierchat/directives/cssTouchAssistant.js
@@ -1,16 +1,17 @@
 angular.module('cssTouchAssistant', [])
 .directive('endTouchFocus', ['$window', function($window) {
 	var endTouch = function(scope, e, attrs) {
-		e.on('click', function() {
+		e[0].addEventListener('touchend', function() {
 			var touchedElement = angular.element(this);
 			var parent = touchedElement.parent();
-			var timeoutCount = 3000;
 
-			touchedElement.css("animation-play-state", "paused");
-			touchedElement.children().css("animation-play-state", "paused");
-			touchedElement[0].blur();
-			$window.setTimeout(function() { touchedElement.css("animation-play-state", "running"); touchedElement.children().css("animation-play-state", "running"); }, timeoutCount);
-		});
+			
+			$window.setTimeout(function() {
+				touchedElement.remove(); 				
+				parent.append(touchedElement);
+			}, 
+			2000);
+		}, true);
 	};
 
 	return {
