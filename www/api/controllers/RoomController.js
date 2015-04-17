c4928dc48389740cca6d832733aa12fdb001e274
Moses <moses.kyle@gmail.com>
2015-04-16 21:04:25 -0400

    Getting started on Room api functionality; Added RoomManager.js & RoomFactory.js

--- www/api/controllers/RoomController.js
+++ www/api/controllers/RoomController.js
@@ -5,6 +5,25 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var RoomManager = require('../modules/RoomManager.js');
+
 module.exports = {
-	
+	create: function(req, res) {
+		var roomName = req.body.name;
+		var roomMgr = new RoomManager();
+
+		roomMgr.enterRoom(roomName).then(function(room) {
+			res.json(room);
+		}, function(err) {
+			res.badRequest(err);
+		});
+	},
+
+	update: function(req, res) {
+
+	},
+
+	destroy: function(req, res) {
+
+	}
 };
