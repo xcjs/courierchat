138384b9aacae3c9b9679cc5f50b3b3dd1021a94
xcjs <zlowery@xcjs.com>
2015-07-02 21:13:58 -0400

    Removed the unnecessary require statement from the beginning of SessionService.

--- www/api/services/SessionService.js
+++ www/api/services/SessionService.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var Q = require('q');
-var UserFactory = require('./UserFactoryService.js');
 
 module.exports = function(session) {
 	var self = this;
@@ -9,7 +8,7 @@ module.exports = function(session) {
 	this.session = session;
 
 	this.login = function(name) {
-    	return UserFactory.findByName(name);
+    	return UserFactoryService.findByName(name);
 	};
 
 	this.logout = function(id) {
@@ -20,7 +19,7 @@ module.exports = function(session) {
 			deferred.resolve();
 		}
 		else if(id === self.session.user.id) {
-		  UserFactory.removeByUser(self.session.user).then(function() {
+		  UserFactoryService.removeByUser(self.session.user).then(function() {
 			self.session.destroy();
 			deferred.resolve();
 		  }, function() {
