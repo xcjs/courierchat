13df50ffe2a4418e13afe3aa05e7f5b4bc6f4a19
xcjs <zlowery@xcjs.com>
2015-09-28 20:31:44 -0400

    Added the environment service to assist with moving between production/development and secure/non-secure protocols.

--- www/assets/js/courierchat/components/shared/environmentService.js
+++ www/assets/js/courierchat/components/shared/environmentService.js
@@ -1,3 +1,14 @@
-/**
- * Created by diaruemnus on 9/25/15.
- */
+courierChat.service('environmentService', ['$location', function($location) {
+	'use strict';
+
+	this.isLocal = function() {
+		if($location.host === 'localhost') return true;
+		return false;
+	};
+
+	this.getSocketProtocol = function() {
+		if($location.protocol() === 'http') return 'ws';
+		return 'wss';
+	};
+
+}]);
