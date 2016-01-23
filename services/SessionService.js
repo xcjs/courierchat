a6a090ff58d86b1016f656b1e48f526a0a9d1a63
xcjs <zlowery@xcjs.com>
2016-01-22 19:36:56 -0500

    Updated the SessionService to use the new configuration file.

--- services/SessionService.js
+++ services/SessionService.js
@@ -1,27 +1,30 @@
 'use strict';
 
 var Q = require('q');
+var config = require('../config.js');
 var crypto = require('crypto');
+var userService = require('./userService.js');
+
 var sha1sum = crypto.createHash('sha1');
 
 module.exports = function() {
 	var self = this;
 
 	this.login = function(name) {
-    	return UserService.create(name);
+    	return userService.create(name);
 	};
 
 	this.createSessionId = function(user) {
-		sha1sum.update(sails.config.session.secret + user.id);
+		sha1sum.update(config.security.secret + user.id);
 		user.token = sha1sum.digest('hex');
 
-		return UserService.update(user);
+		return userService.update(user);
 	};
 
 	this.logout = function(user) {
 		var deferred = Q.defer();
 
-		UserService.removeByUser(user).then(function() {
+		userService.removeByUser(user).then(function() {
 			deferred.resolve();
 		}, function() {
 			deferred.reject('We had trouble logging you out. Please try again in a few minutes!');
