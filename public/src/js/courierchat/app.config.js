b291766404b23d90e3223ba4d0a56bcc605574c4
xcjs <zlowery@xcjs.com>
2016-03-16 19:24:13 -0400

    Wrapped the app declaration in anonymous function with a use strict statement and split the config out to a separate file.

---
+++ public/src/js/courierchat/app.config.js
@@ -0,0 +1,36 @@
+(function() {
+	'use strict';
+
+	angular.module('courierChat').config(['$stateProvider', '$locationProvider', '$httpProvider',
+		function($stateProvider, $locationProvider, $httpProvider) {
+			'use strict';
+
+			var appBase = '/js/courierchat/';
+			var components = appBase + 'components/';
+
+			$locationProvider.html5Mode(true);
+
+			$httpProvider.interceptors.push('authInterceptor');
+
+			$stateProvider
+				.state('home', {
+					url: '/',
+					templateUrl: components + 'login/login.html',
+					controller: 'loginController'
+				})
+				.state('login', {
+					url: '/login',
+					templateUrl: components + 'login/login.html',
+					controller: 'loginController'
+				})
+				.state('about',  {
+					url: '/about',
+					templateUrl: components + 'about/about.html'
+				})
+				.state('rooms', {
+					url: '/rooms',
+					templateUrl: components + 'rooms/rooms.html',
+					controller: 'roomsController'
+				});
+		}]);
+})();
--- public/src/js/courierchat/app.js
+++ public/src/js/courierchat/app.js
@@ -1,34 +1,8 @@
-var courierChat = angular.module('courierChat', ['ui.router', 'ngResource']);
-
-courierChat.config(['$stateProvider', '$locationProvider', '$httpProvider',
-	function($stateProvider, $locationProvider, $httpProvider) {
+(function() {
 	'use strict';
 
-	var appBase = '/js/courierchat/';
-	var components = appBase + 'components/';
-
-	$locationProvider.html5Mode(true);
-
-	$httpProvider.interceptors.push('authInterceptor');
-
-    $stateProvider
-    .state('home', {
-		url: '/',
-		templateUrl: components + 'login/login.html',
-  		controller: 'loginController'
-    })
-    .state('login', {
-  		url: '/login',
-  		templateUrl: components + 'login/login.html',
-  		controller: 'loginController'
-    })
-    .state('about',  {
-		url: '/about',
-		templateUrl: components + 'about/about.html'
-  	})
-    .state('rooms', {
-  		url: '/rooms',
-  		templateUrl: components + 'rooms/rooms.html',
-		controller: 'roomsController'
-  	});
-}]);
+	angular.module('courierChat', [
+		'ngResource',
+		'ui.router'
+	]);
+})();
