1098e4c8495773f92bbdac90b9f435c712ad9f2a
xcjs <zlowery@xcjs.com>
2016-03-16 19:08:41 -0400

    Added default .jshintrc

--- Gulpfile.js
+++ Gulpfile.js
@@ -116,7 +116,7 @@ gulp.task('minVendorJs', function() {
 
 gulp.task('jshint', function() {
 	return gulp.src('public/src/js/**/*.js')
-		.pipe(jshint())
+		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('jshint-stylish'));
 });
 
