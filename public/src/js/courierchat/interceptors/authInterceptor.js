e389fbdefadbb7fd41848fdea3a6bf3a0d8a17e7
xcjs <zlowery@xcjs.com>
2016-03-16 19:49:34 -0400

    Corrected jshint errors for authInterceptor.

--- public/src/js/courierchat/interceptors/authInterceptor.js
+++ public/src/js/courierchat/interceptors/authInterceptor.js
@@ -1,12 +1,16 @@
-courierChat.factory('authInterceptor',
-	['$rootScope', function($rootScope)	{
-		return {
-			request: function($config) {
-				if($rootScope.user && $rootScope.user.token) {
-					$config.headers['courierchat-auth-token'] = $rootScope.user.token;
-				}
+(function() {
+	'use strict';
 
-				return $config;
-			}
-		};
-	}]);
+	angular.module('courierChat').factory('authInterceptor',
+		['$rootScope', function ($rootScope) {
+			return {
+				request: function ($config) {
+					if ($rootScope.user && $rootScope.user.token) {
+						$config.headers['courierchat-auth-token'] = $rootScope.user.token;
+					}
+
+					return $config;
+				}
+			};
+		}]);
+})();
