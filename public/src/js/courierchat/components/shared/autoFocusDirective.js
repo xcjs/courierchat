120a2c142012adafdcd97ef3b3001c47c7d6640f
xcjs <zlowery@xcjs.com>
2016-03-16 20:38:30 -0400

    Solved jshint issues with the autoFocusDirective.

--- public/src/js/courierchat/components/shared/autoFocusDirective.js
+++ public/src/js/courierchat/components/shared/autoFocusDirective.js
@@ -1,9 +1,13 @@
-courierChat.directive('autoFocus', function() {
-	return {
-		link: {
-			post: function postLink(scope, element, attr) {
-				element[0].focus();
+(function() {
+	'use strict';
+
+	angular.module('courierChat').directive('autoFocus', function() {
+		return {
+			link: {
+				post: function postLink(scope, element) {
+					element[0].focus();
+				}
 			}
-		}
-	};
-});
+		};
+	});
+})();
