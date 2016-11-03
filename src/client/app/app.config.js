9f8b0d5d435daee45e28b9e248f53667f34763e2
Zackary Lowery <zlowery@xcjs.com>
2016-11-02 20:18:21 -0400

    Cleaned up the app.config syntax per JohnPapa.

--- src/client/app/app.config.js
+++ src/client/app/app.config.js
@@ -1,35 +1,36 @@
 (function() {
 	'use strict';
 
-	angular.module('courierChat').config(['$stateProvider', '$locationProvider', '$httpProvider',
-		function($stateProvider, $locationProvider, $httpProvider) {
-			$locationProvider.html5Mode(true);
+	angular.module('courierChat').config(config);
 
-			$httpProvider.interceptors.push('authInterceptor');
+	function config($stateProvider, $locationProvider, $httpProvider) {
+		$locationProvider.html5Mode(true);
 
-			$stateProvider
-				.state('home', {
-					url: '/',
-					templateUrl: 'app/login/login.html',
-					controller: 'LoginController',
-					controllerAs: 'vm'
-				})
-				.state('login', {
-					url: '/login',
-					templateUrl: 'app/login/login.html',
-					controller: 'loginController',
-					controllerAs: 'vm'
-				})
-				.state('about',  {
-					url: '/about',
-					templateUrl: 'app/about/about.html',
-					controllerAs: 'vm'
-				})
-				.state('rooms', {
-					url: '/rooms',
-					templateUrl: 'app/rooms/rooms.html',
-					controller: 'roomsController',
-					controllerAs: 'vm'
-				});
-		}]);
+		$httpProvider.interceptors.push('authInterceptor');
+
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'app/login/login.html',
+				controller: 'LoginController',
+				controllerAs: 'vm'
+			})
+			.state('login', {
+				url: '/login',
+				templateUrl: 'app/login/login.html',
+				controller: 'loginController',
+				controllerAs: 'vm'
+			})
+			.state('about',  {
+				url: '/about',
+				templateUrl: 'app/about/about.html',
+				controllerAs: 'vm'
+			})
+			.state('rooms', {
+				url: '/rooms',
+				templateUrl: 'app/rooms/rooms.html',
+				controller: 'roomsController',
+				controllerAs: 'vm'
+			});
+	}
 })();
